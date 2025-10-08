5dfb4179b5263b9535518d90c75bd591a6c8f75e
CodingWithHammad <codingwithhammad786@gmail.com>
2025-10-08 23:08:57 +0530

    working on user.controller's 1. create register controller 2. create login controller 3. create forgetPassword controller

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -6,10 +6,112 @@ import generateToken from "../utlis/generateToken.js"
 const transporter = nodemailer.createTransport(
     {
         auth: {
-            EMAIL_USER: process.env.EMAIL_USER,
-            EMAIL_PASSWORD: process.env.EMAIL_PASSWORD
+            user: process.env.EMAIL_USER,
+            pass: process.env.EMAIL_PASSWORD
         },
         service: "gmail"
     }
 )
 
+export const register = async (req, res) => {
+    try {
+
+        const { name, email, password, role } = req.body;
+        const user = await User.findOne({ email });
+        if (user) console.log("User already exist...")
+
+        const newUser = User.create({
+            name: name,
+            password: password,
+            email: email,
+            role: role
+        })
+
+        console.log({
+            message: "New user successfully created...",
+            token: generateToken(user?._id),
+            newUser
+        })
+
+        res.status(201).json({
+            message: "New user successfully created...",
+            token: generateToken(user?._id),
+            newUser
+        })
+
+    } catch (error) {
+        console.log("Error present inside the register controller, error is :" + error);
+        res.status(500).json({
+            message: "Error present inside the register controller ...",
+            error
+        })
+    }
+}
+
+export const login = async (req, res) => {
+    try {
+
+        const { email, password } = req.body
+        const user = await User.findOne({ email })
+        if (!user) res.status(400).json({ message: "User is not found!..." })
+
+        const isMatch = await user.matchPassword(password);
+        if (!isMatch) console.log("Invalid credentials ...");
+
+        console.log({
+            message: `${user?.name} login successfully ...`,
+            user
+        })
+
+        res.status(200).json({
+            message: `${user?.name} login successfully ...`,
+            token: generateToken(user?._id),
+            user
+        })
+
+    } catch (error) {
+        console.log("Error present inside the login controller : " + error);
+        res.status(500).json({
+            message: "Error present inside the login controller",
+            error
+        })
+    }
+}
+
+export const forgetPassword = async (req, res) => {
+    try {
+
+        const { email } = req.body
+        const user = await User.findOne({ email })
+        if (!user) res.status(400).json({ message: "User is not found..." })
+
+        const otp = Math.floor(100000 + Math.random() * 90000).toString()
+        user?.otp = otp
+        user?.otpExpiry = Date.now() + 10 * 60 * 1000;
+        await user.save();
+
+        await transporter.sendMail({
+            from: `"Digital Learning Platform" <${process.env.EMAIL_USER}>`,
+            to: user.email,
+            subject: "Password Reset OTP",
+            text: `Your OTP for password reset is ${otp}. It will expire in 10 minutes.`,
+        });
+
+        console.log({
+            message: "OTP sent to your email",
+            user
+        })
+
+        res.status(200).json({
+            message: "OTP sent to your email",
+            user
+        });
+
+    } catch (error) {
+        console.log("Error present inside the forget password controller : " + error);
+        res.status(500).json({
+            message: "Error present inside the forget password controller",
+            error
+        })
+    }
+}
