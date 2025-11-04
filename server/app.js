53e0f9ab1bf08d7dea9806a1ca73bb8a52f95859
CodingWithHammad <codingwithhammad786@gmail.com>
2025-11-04 23:19:34 +0530

    working on frontened

--- server/app.js
+++ server/app.js
@@ -32,6 +32,7 @@ app.use("/api/auth", authRoutes);
 // ✅ DB + Server
 connectDB();
 
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`🚀 Server running at http://localhost:${PORT}/`);
