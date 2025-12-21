96ca41c4bde4c39cb5a606b52eece3e593380d79
CodingWithHammad <codingwithhammad786@gmail.com>
2025-12-21 23:38:56 +0530

    working on quiz section

--- server/models/Quiz.model.js
+++ server/models/Quiz.model.js
@@ -44,13 +44,6 @@ const UserSchema = new Schema(
     }
 );
 
-// ✅ Hash password before saving
-UserSchema.pre("save", async function (next) {
-    if (!this.isModified("password")) return next();
-    const salt = await bcrypt.genSalt(10);
-    this.password = await bcrypt.hash(this.password, salt);
-    next();
-});
 
 
 
