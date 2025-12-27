ce9e403d9963191469ec7ec7a00cc430b5c4f7dd
CodingWithHammad <codingwithhammad786@gmail.com>
2025-12-27 23:17:59 +0530

    solving issue in app file

--- server/app.js
+++ server/app.js
@@ -1,12 +1,10 @@
 import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
-
 import authRoutes from "./routes/authRoutes.js";
 import { connectDB } from "./config/db.js";
 
 dotenv.config();
-
 const app = express();
 
 // ✅ Middleware
@@ -32,5 +30,4 @@ connectDB();
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`🚀 Server running at http://localhost:${PORT}/`);
-  
 });
